26a2e0b4280f08b20feabc1018b6dbfeb19061a5
rikiyay <rickdom2610@gmail.com>
2020-04-06 22:47:25 -0700

    added results

--- README.md
+++ README.md
@@ -5,19 +5,69 @@ In this repo, we investigate:
 - Efficacy of Lightswitch Method (described [here](https://covid-measures.github.io/))  
   
 ---  
+
+- No Social Distancing  
+![](simulations/flattenthecurve_100.gif)  
+![](overlayeds/overlay_100.png)  
+
+- Social Distancing: Whole Period  
+
+ - Activity Level 70%  
+ - ![](simulations/flattenthecurve_70.gif)  
+
+ - Activity Level 40%  
+ - ![](simulations/flattenthecurve_40.gif)  
+
+![](concats/concat_overlayed_wholeperiod.png)  
+
+- Social Distancing: Early Stop  
+
+ - Activity Level 40% -> 100%  
+ - ![](simulations/flattenthecurve_stop_shortterm.gif)  
+
+ - Activity Level 40% -> 70%  
+ - ![](simulations/flattenthecurve_stop_shortterm70.gif)  
+
+![](concats/concat_overlayed_stop_shortterm.png)  
+
+- Social Distancing: Mid Stop  
+
+ - Activity Level 40% -> 100%  
+ - ![](simulations/flattenthecurve_stop_midterm.gif)  
+
+ - Activity Level 40% -> 70%  
+ - ![](simulations/flattenthecurve_stop_midterm70.gif)  
+
+![](concats/concat_overlayed_stop_midterm.png)  
+
+- Social Distancing: Late Stop  
+
+ - Activity Level 40% -> 100%  
+ - ![](simulations/flattenthecurve_stop_longterm.gif)  
+
+ - Activity Level 40% -> 70%  
+ - ![](simulations/flattenthecurve_stop_longterm70.gif)  
+
+![](concats/concat_overlayed_stop_longterm.png)  
+
+- Social Distancing: Lightswitch  
+
+ - Activity Level 40% -> 100%  
+ - ![](simulations/flattenthecurve_lightswitch.gif)  
+
+ - Activity Level 40% -> 70%  
+ - ![](simulations/flattenthecurve_lightswitch70.gif)  
+
+![](concats/concat_overlayed_lightswitch.png)  
+
+---  
   
-Link to interactive:  
+Link to abbreviated version of interactive:  
 - [Notebook](https://nbviewer.jupyter.org/github/rikiyay/covid19/blob/master/flatten_the_curve.ipynb?flush_cache=true)  
 - [HTML](https://htmlpreview.github.io/?https://github.com/rikiyay/covid19/blob/master/flatten_the_curve.html)  
 - [Slide](https://htmlpreview.github.io/?https://github.com/rikiyay/covid19/blob/master/flatten_the_curve.slides.html)  
   
 ---  
   
-Replication and extension of the simulation in [this Washington Post article](https://www.washingtonpost.com/graphics/2020/world/corona-simulator/)  
-Reference to [this implementation](https://github.com/xnx/collision) of elastic collision  
-
-<!-- No Social Distancing  
-![](flattencurve_abbrev_dt025.gif)  
-
-Social Distancing: 30% of Baseline Contact Ratio  
-![](flattencurve_abbrev_dt008.gif)   -->
+Inspired by the simulation in [this Washington Post article](https://www.washingtonpost.com/graphics/2020/world/corona-simulator/) by Harry Stevens  
+Reference to [this implementation](https://github.com/xnx/collision) of elastic collision by Christian Hill  
--- flatten_the_curve.py
+++ flatten_the_curve.py
@@ -1,3 +1,7 @@
+"""
+referred to https://github.com/xnx/collision for inplementation of elastic collision
+"""
+
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.patches import Circle
@@ -8,12 +12,10 @@ from matplotlib import colors as mcolors
 
 colors = dict(mcolors.BASE_COLORS, **mcolors.CSS4_COLORS)
 class Particle:
-    """A class representing a two-dimensional particle."""
             
     def __init__(self, x, y, vx, vy, radius=0.01, prop=0):
-        """Initialize the particle's position, velocity, and radius.
-        Any key-value pairs passed in the styles dictionary will be passed
-        as arguments to Matplotlib's Circle patch constructor.
+        """
+        Initialize the particle's position, velocity, radius, and initial property.
         """
 
         self.r = np.array((x, y))
@@ -70,18 +72,19 @@ class Particle:
         self.r += self.v * dt
 
 class Simulation:
-    """A class for a simple hard-circle molecular dynamics simulation.
+    """
+    A class for a simple infectous disease spreading simulation.
     The simulation is carried out on a square domain: 0 <= x < 1, 0 <= y < 1.
     """
 
     ParticleClass = Particle
 
-    def __init__(self, n, radius=0.01, prop=0, transmission_rate=0.6, disease_duration=800, death_rate=0.4, dt=0.01, dttype=None):
-        """Initialize the simulation with n Particles with radii radius.
-        radius can be a single value or a sequence with n values.
-        Any key-value pairs passed in the styles dictionary will be passed
-        as arguments to Matplotlib's Circle patch constructor when drawing
-        the Particles.
+    def __init__(self, n, radius=0.012, prop=0, transmission_rate=0.8, disease_duration=120, death_rate=0.2, dt=0.036, dttype=None):
+        """
+        Initialize the simulation with
+            n Particles with radii radius,
+            disease transmission rate, diseae duration, and death rate,
+            particle activity level by dt.
         """
 
         self.init_particles(n, radius, prop)
@@ -96,6 +99,7 @@ class Simulation:
         self.infected = np.array(0.)
         self.recovered = np.array(0.)
         self.dead = np.array(0.)
+        self.comb = {}
 
     def place_particle(self, rad, prop):
         # Choose x, y so that the Particle is entirely inside the
@@ -118,7 +122,8 @@ class Simulation:
         return False
 
     def init_particles(self, n, radius, prop):
-        """Initialize the n Particles of the simulation.
+        """
+        Initialize the n Particles of the simulation.
         Positions and velocities are chosen randomly; radius can be a single
         value or a sequence with n values.
         """
@@ -137,10 +142,12 @@ class Simulation:
         self.n = n
         self.particles = []
         for i, rad in enumerate(radius):
-            if i == 0:
+            # Set first 3 particles' property as 1 (infected).
+            if i < 3:
                 self.place_particle(rad, prop=1)
             else:
                 # Try to find a random initial position for this particle.
+                # Set the other particles' property as 0 (healthy).
                 while not self.place_particle(rad, prop):
                     pass
 
@@ -163,8 +170,9 @@ class Simulation:
         else:
             pass
             
-    def handle_collisions(self):
-        """Detect and handle any collisions between the Particles.
+    def handle_collisions(self, time):
+        """
+        Detect and handle any collisions between the Particles.
         When two Particles collide, they do so elastically: their velocities
         change such that both energy and momentum are conserved.
         """ 
@@ -172,18 +180,53 @@ class Simulation:
         # We're going to need a sequence of all of the pairs of particles when
         # we are detecting collisions. combinations generates pairs of indexes
         # into the self.particles list of Particles on the fly.
+        comb = []
         pairs = combinations(range(self.n), 2)
-        for i,j in pairs:
-            if self.particles[i].overlaps(self.particles[j]):
-                self.change_velocities(self.particles[i], self.particles[j])
-                
-                props = [0, 1]
-                if self.particles[i].prop==1 and self.particles[j].prop==0:
-                    self.particles[j].prop = np.random.choice(props, p=[1-self.transmission_rate, self.transmission_rate])
-                    self.circles[j].set_color(self.particles[j].prop_dict[self.particles[j].prop])
-                elif self.particles[i].prop==0 and self.particles[j].prop==1:
-                    self.particles[i].prop = np.random.choice(props, p=[1-self.transmission_rate, self.transmission_rate])
-                    self.circles[i].set_color(self.particles[i].prop_dict[self.particles[i].prop])
+        
+        # Set patience for handling collision as 2 if time (frame idx) >= 2.
+        if time == 0:
+            for i,j in pairs:
+                if self.particles[i].overlaps(self.particles[j]):
+                    self.change_velocities(self.particles[i], self.particles[j])
+                    comb.append((i, j))
+                    
+                    props = [0, 1]
+                    if self.particles[i].prop==1 and self.particles[j].prop==0:
+                        self.particles[j].prop = np.random.choice(props, p=[1-self.transmission_rate, self.transmission_rate])
+                        self.circles[j].set_color(self.particles[j].prop_dict[self.particles[j].prop])
+                    elif self.particles[i].prop==0 and self.particles[j].prop==1:
+                        self.particles[i].prop = np.random.choice(props, p=[1-self.transmission_rate, self.transmission_rate])
+                        self.circles[i].set_color(self.particles[i].prop_dict[self.particles[i].prop])
+        elif time == 1:
+            for i,j in pairs:
+                if self.particles[i].overlaps(self.particles[j]):
+                    comb.append((i, j))
+                    if (i, j) not in self.comb[time-1]:
+                        self.change_velocities(self.particles[i], self.particles[j])
+                        
+                        props = [0, 1]
+                        if self.particles[i].prop==1 and self.particles[j].prop==0:
+                            self.particles[j].prop = np.random.choice(props, p=[1-self.transmission_rate, self.transmission_rate])
+                            self.circles[j].set_color(self.particles[j].prop_dict[self.particles[j].prop])
+                        elif self.particles[i].prop==0 and self.particles[j].prop==1:
+                            self.particles[i].prop = np.random.choice(props, p=[1-self.transmission_rate, self.transmission_rate])
+                            self.circles[i].set_color(self.particles[i].prop_dict[self.particles[i].prop])
+        else:
+            for i,j in pairs:
+                if self.particles[i].overlaps(self.particles[j]):
+                    comb.append((i, j))
+                    if ((i, j) not in self.comb[time-1]) and ((i, j) not in self.comb[time-2]):
+                        self.change_velocities(self.particles[i], self.particles[j])
+                        
+                        props = [0, 1]
+                        if self.particles[i].prop==1 and self.particles[j].prop==0:
+                            self.particles[j].prop = np.random.choice(props, p=[1-self.transmission_rate, self.transmission_rate])
+                            self.circles[j].set_color(self.particles[j].prop_dict[self.particles[j].prop])
+                        elif self.particles[i].prop==0 and self.particles[j].prop==1:
+                            self.particles[i].prop = np.random.choice(props, p=[1-self.transmission_rate, self.transmission_rate])
+                            self.circles[i].set_color(self.particles[i].prop_dict[self.particles[i].prop])
+        
+        self.comb[time] = comb
 
     def handle_boundary_collisions(self, p):
         """Bounce the particles off the walls elastically."""
@@ -201,36 +244,25 @@ class Simulation:
             p.y = 1-p.radius
             p.vy = -p.vy
 
-    def apply_forces(self):
-        """Override this method to accelerate the particles."""
-        pass
-
-    def advance_animation(self):
+    def advance_animation(self, time):
         """Advance the animation by dt, returning the updated Circles list."""
 
         for i, p in enumerate(self.particles):
             p.advance(self.dt)
             self.handle_boundary_collisions(p)
             self.circles[i].center = p.r
-        self.handle_collisions()
-        self.apply_forces()
+        self.handle_collisions(time)
         return self.circles
-
-    def advance(self):
-        """Advance the animation by dt."""
-        for i, p in enumerate(self.particles):
-            p.advance(self.dt)
-            self.handle_boundary_collisions(p)
-        self.handle_collisions()
-        self.apply_forces()
 
     def init(self):
         """Initialize the Matplotlib animation."""
 
+        # Initialize particles
         self.circles = []
         for particle in self.particles:
             self.circles.append(particle.draw(self.ax1))
         
+        # initialize texts
         self.texts = []
         self.texts.append(self.ax2.text(0.25,0.725, 'healthy', ha="left", va="center", fontsize=14, color='black'))
         self.texts.append(self.ax2.text(0.75,0.725, '', ha="right", va="center", fontsize=14, color='silver'))
@@ -241,31 +273,78 @@ class Simulation:
         self.texts.append(self.ax2.text(0.25,0.275, 'dead', ha="left", va="center", fontsize=14, color='black'))
         self.texts.append(self.ax2.text(0.75,0.275, '', ha="right", va="center", fontsize=14, color='dimgray'))
         
+        # Initialize bars
         self.bar = self.ax3.bar([], [], width=1)
     
         return self.circles + self.texts + list(self.bar)
 
     def animate(self, i):
-        """The function passed to Matplotlib's FuncAnimation routine."""
+        """
+        The function passed to Matplotlib's FuncAnimation routine.
+        dttype can be one of followings: None, stop_short, stop_mid, stop_long, lightswitch.
+        """
+        
+        if self.dttype == 'stop_short': # duration of shelter-in-place: 200
+            if i >= 200:
+                self.dt = 0.036 # 0.25
 
-        if self.dttype == 'stop1/4':
-            if i >= 150:
-                self.dt = 0.03
-        elif self.dttype == 'stop1/2':
+        elif self.dttype == 'stop_mid': # duration of shelter-in-place: 300
             if i >= 300:
-                self.dt = 0.03
-        elif self.dttype == 'stop3/4':
-            if i >= 450:
-                self.dt = 0.03
-        elif self.dttype == 'lightswitch':
-            if i >= 150 and i < 300:
-                self.dt = 0.03
-            elif i >= 300 and i < 450:
-                self.dt = 0.01
-            elif i >= 450:
-                self.dt = 0.03
+                self.dt = 0.036 # 0.25
 
-        self.advance_animation()
+        elif self.dttype == 'stop_long': # duration of shelter-in-place: 400
+            if i >= 400:
+                self.dt = 0.036 # 0.25
+
+        elif self.dttype == 'lightswitch': # duration of shelter-in-place: 320
+            if i >= 80 and i < 120:
+                self.dt = 0.036 # or 0.25
+            elif i >= 120 and i < 200:
+                self.dt = 0.015
+            elif i >= 200 and i < 240:
+                self.dt = 0.036 # or 0.25
+            elif i >= 240 and i < 320:
+                self.dt = 0.015
+            elif i >= 320 and i < 360:
+                self.dt = 0.036 # or 0.25
+            elif i >= 360 and i < 440:
+                self.dt = 0.015
+            elif i >= 440:
+                self.dt = 0.036
+
+        # elif self.dttype == 'lightswitch4_21': # duration of shelter-in-place: 400
+        #     if i >= 200 and i < 300:
+        #         self.dt = 0.025 # or 0.36
+        #     elif i >= 300 and i < 500:
+        #         self.dt = 0.015
+        #     elif i >= 500:
+        #         self.dt = 0.036
+
+        # elif self.dttype == 'lightswitch6_21': # duration of shelter-in-place: 480
+        #     if i >= 160 and i < 240:
+        #         self.dt = 0.025 # or 0.36
+        #     elif i >= 240 and i < 400:
+        #         self.dt = 0.015
+        #     elif i >= 400 and i < 480:
+        #         self.dt = 0.025 # or 0.36
+        #     elif i >= 480 and i < 640:
+        #         self.dt = 0.015
+        #     elif i >= 640:
+        #         self.dt = 0.036
+
+        # elif self.dttype == 'lightswitch6_21_frequent': # duration of shelter-in-place: 240
+        #     if i >= 80 and i < 120:
+        #         self.dt = 0.025 # or 0.36
+        #     elif i >= 120 and i < 200:
+        #         self.dt = 0.015
+        #     elif i >= 200 and i < 240:
+        #         self.dt = 0.025 # or 0.36
+        #     elif i >= 240 and i < 320:
+        #         self.dt = 0.015
+        #     elif i >= 320:
+        #         self.dt = 0.036
+                
+        self.advance_animation(i)
         healthy = 0
         infected = 0
         recovered = 0
@@ -300,6 +379,11 @@ class Simulation:
         self.texts[5].set_text(str(recovered))
         self.texts[7].set_text(str(dead))
         
+        self.ax3.clear()
+        self.ax3.set_xlim(0, self.nframes)
+        self.ax3.set_ylim(0, 1)
+        self.ax3.xaxis.set_ticks([])
+        self.ax3.yaxis.set_ticks([])
         self.bar = self.ax3.bar([*range(len(self.healthy))], self.infected, width=1, color = colors['indianred'])
         self.bar = self.ax3.bar([*range(len(self.healthy))], self.healthy, width=1, bottom=self.infected, color = colors['silver'])
         self.bar = self.ax3.bar([*range(len(self.healthy))], self.recovered, width=1, bottom=self.infected+self.healthy, color = colors['cadetblue'])
@@ -309,12 +393,14 @@ class Simulation:
 
     def setup_animation(self):
 
+        # Initialize fig and axes
         self.fig = plt.figure(figsize=(10, 5))
         gs = GridSpec(2, 2, figure=self.fig)
         self.ax1 = self.fig.add_subplot(gs[:, 0])
         self.ax2 = self.fig.add_subplot(gs[0, 1])
         self.ax3 = self.fig.add_subplot(gs[1, 1])
         
+        # Initialize ax1 for particles
         for s in ['top','bottom','left','right']:
             self.ax1.spines[s].set_linewidth(1)
         self.ax1.set_xlim(0, 1)
@@ -322,8 +408,10 @@ class Simulation:
         self.ax1.xaxis.set_ticks([])
         self.ax1.yaxis.set_ticks([])
         
+        # Initialize ax2 for texts
         self.ax2.axis("off")
         
+        # Initialize ax3 for bars
         self.ax3.spines['right'].set_visible(False)
         self.ax3.spines['top'].set_visible(False)
         self.ax3.set_xlim(0, self.nframes)
@@ -333,54 +421,42 @@ class Simulation:
         
     def save_or_show_animation(self, anim, save, filename='flattencurve.gif'):
         if save:
-            # 1
-            Writer = animation.ImageMagickFileWriter(fps=30)
-            anim.save('flattencurve.gif', writer=Writer)
-            # anim.save('flattencurve.mp4', writer=Writer)
-            # 2
-            # anim.save('flattencurve.mp4', fps=30)
-            # 3
-            # anim.save('flattencurve.gif', writer='imagemagick', fps=30)
-            # 4
-            # Writer = animation.FFMpegFileWriter(fps=30)            
+            if filename[-3:] == 'gif':
+                # GIF
+                Writer = animation.ImageMagickFileWriter(fps=60)
+                anim.save(filename, writer=Writer)
+            elif filename[-3:] == 'mp4':
+            # MP4
+                Writer = animation.FFMpegFileWriter(fps=60)
+                anim.save(filename, writer=Writer)  
             
         else:
             plt.show() 
 
-    def do_animation(self, save=False, nframes=600, interval=1000/30, filename='flattencurve.gif'):
-        """Set up and carry out the animation of the molecular dynamics.
-        To save the animation as a MP4 movie, set save=True.
+    def do_animation(self, save=False, nframes=900, interval=1000/60, filename='flattencurve.gif'):
+        """
+        Set up and carry out the animation.
+        To save the animation as a GIF or MP4 movie, set save=True and specify filename.
         """
 
         self.nframes = nframes
         self.setup_animation()
         anim = animation.FuncAnimation(self.fig, self.animate,
-                init_func=self.init, frames=nframes, interval=interval, blit=True)
+                init_func=self.init, frames=nframes, interval=interval, blit=False)
         self.save_or_show_animation(anim, save, filename)
+        
         return anim
 
 if __name__ == '__main__':
-    nparticles = 160
-    radii = 0.015
+    nparticles = 180
+    radii = 0.012
     prop = 0
-
-    nframes = 600
+    nframes = 900
     transmission_rate = 0.8
-    disease_duration = 200
+    disease_duration = 120
     death_rate = 0.2
-    dt = 0.01 # 0.03, 0.01
-    dttype = None # 'stop1/4', 'stop1/2', 'stop3/4', 'lightswitch'
-
-    # Abbreviated version (.gif)
-    # nparticles = 20
-    # radii = 0.05
-    # prop = 0
-    # nframes = 100
-    # transmission_rate = 0.8
-    # disease_duration = 30
-    # death_rate = 0.4
-    # dt = 0.08 # 0.25, 0.08
-    # dttype = None # 'stop1/4', 'stop1/2', 'stop3/4', 'lightswitch'
-
+    dt = 0.015 # 0.036, 0.025
+    dttype = 'lightswitch' # None, stop_short, stop_mid, stop_long, lightswitch
+    
     sim = Simulation(nparticles, radii, prop, transmission_rate, disease_duration, death_rate, dt, dttype)
-    sim.do_animation(save=True, nframes=nframes)
+    sim.do_animation(save=True, nframes=nframes, filename='flattenthecurve.gif')
